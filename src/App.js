444723bf7c339140f5e00a7d73d6c97409a67102
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2022-05-08 21:12:57 +0700

    example: useEffect

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import axios from 'axios';
 import './App.css'
 function App () {
   const [data, setData] = useState([])
+  const [filter, setFilter] = useState('')
+  const [filterResult, setFilterResult] = useState([])
+
+  const searchPerson = (value) => {
+    setFilter(value)
+    if (filter !== '') {
+      const filteredData = data.filter((item) => {
+        return Object.values(item).join(''.toLowerCase()).includes(filter.toLowerCase())
+      })
+      setFilterResult(filteredData)
+    } else {
+      setFilterResult(data)
+    }
+  }
+
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
+    axios.get('https://jsonplaceholder.typicode.com/users/')
       .then(res => {
         setData(res.data)
       })
-    return () => {
-      setData([])
-    }
   }, [])
 
   return (
     <div className='App'>
-      {data.map((val, i) => (
-        <div key={i}>
-          <p >{val.name}</p>
-        </div>
-      ))}
+      <input type="text" onChange={(e) => searchPerson(e.target.value)} />
+
+      {
+        filter.length > 0 ? (
+          filterResult.map((item) => (
+            <div key={item.id}>
+              <h1>{item.name}</h1>
+            </div>
+          ))
+        ) : (
+          data.map((val, i) => (
+            <div key={i}>
+              <p >{val.name}</p>
+            </div>
+          ))
+        )
+      }
+
     </div>
   );
 }
